e9cddb6ee128259ab3b3cd7a25f3a8f73e9583a8
Aaron <asintayehu@proton.me>
2025-02-16 14:45:26 -0500

    Realized OpenWeatherAPI isn't free anymore, opting for Tomorrow weather api

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import pgeocode
 import requests
-import re
 
-text = requests.get("https://www.linkedin.com").text
+api_key = "e2a92538221d485a56aae5456e317793"
 
-opening_tag = True
-output_str = ""
+zip_code = input("Enter zip code: ")
+nomi = pgeocode.Nominatim('us')
+query = nomi.query_postal_code(zip_code)
+lat = query["latitude"]
+lon = query["longitude"]
 
-print(output_str)
-        
+x = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={e2a92538221d485a56aae5456e317793}")
 
-
+print(x)
