5a318e21cf0b844a6aefffc50f0110310c737208
Aaron <asintayehu@proton.me>
2025-02-17 07:46:50 -0500

    learned sql, wil collect different data and change api

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import json
 import math
+import psycopg2
 
 def convert_to_f(temp):
     return (temp * 9/5) + 32
 
-api_key = ""
+# api key: 5z9CQ7slmvalWcMSxJQOj2qlDQB7nB1c
+
+api_key = "5z9CQ7slmvalWcMSxJQOj2qlDQB7nB1c"
 
 location = input("Enter city: ")
 
